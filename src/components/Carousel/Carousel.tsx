5db6747acf979c094893d967c1bc4714a62d2119
phuoc <phuocnguyen@ariadirect.com>
2023-06-05 01:44:18 +0700

    refactor

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,47 +1,10 @@
 import React, { useRef } from 'react'
-import { styled } from '@mui/material/styles'
-
 import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-
 import { CircularProgress, Box, Typography } from '@mui/material'
 
 import { MovieData } from 'src/models/movie'
 import MovieCard from '../MovieCard/MovieCard'
-
-const CarouselContainer = styled('div')(({ theme }) => ({
-  marginBottom: theme.spacing(4),
-  position: 'relative',
-
-  '.arrow': {
-    fontSize: 30,
-    color: 'black',
-    position: 'absolute',
-    top: '44%',
-    transform: 'translateY(-50%)',
-    cursor: 'pointer',
-    opacity: 0.5,
-    zIndex: 1,
-    display: 'none',
-
-    [theme.breakpoints.up('md')]: {
-      display: 'block'
-    },
-
-    '&:hover': {
-      opacity: 0.8
-    }
-  },
-
-  '.carouselLeftNav': {
-    left: 30
-  },
-
-  '.carouselRighttNav': {
-    right: 30
-  }
-}))
-
+import { useAppSelector } from 'src/hooks/hooks'
 interface CarouselProps {
   data: MovieData[] | undefined
   loading: boolean | string | null
@@ -51,28 +14,12 @@ interface CarouselProps {
 
 const Carousel: React.FC<CarouselProps> = ({ data, loading, endpoint, title }) => {
   const carouselContainer = useRef<HTMLDivElement>(null)
-  const { url } = useSelector((state: any) => state.home)
+  const { url } = useAppSelector((state) => state.home)
   const navigate = useNavigate()
 
-  // const navigation = (dir: string) => {
-  //   const container = carouselContainer.current
-
-  //   const scrollAmount =
-  //     dir === 'left'
-  //       ? container?.scrollLeft - (container.offsetWidth + 20)
-  //       : container?.scrollLeft + (container.offsetWidth + 20)
-
-  //   container?.scrollTo({
-  //     left: scrollAmount,
-  //     behavior: 'smooth'
-  //   })
-  // }
-
   return (
-    <CarouselContainer>
+    <Box>
       {title && <Typography variant='h4'>{title}</Typography>}
-      {/* <BsFillArrowLeftCircleFill className='carouselLeftNav arrow' onClick={() => navigation('left')} />
-        <BsFillArrowRightCircleFill className='carouselRighttNav arrow' onClick={() => navigation('right')} /> */}
       {!loading ? (
         <Box
           sx={(theme) => ({
@@ -100,7 +47,7 @@ const Carousel: React.FC<CarouselProps> = ({ data, loading, endpoint, title }) =
           <CircularProgress />
         </Box>
       )}
-    </CarouselContainer>
+    </Box>
   )
 }
 
