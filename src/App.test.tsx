9095aeeca44b5594adb0c7f4fc4ce2167204c1eb
phuoc <phuocnguyen@ariadirect.com>
2023-06-04 17:46:45 +0700

    feat(unit-test): setup unit Testing with Vitest, and React Testing Library

---
+++ src/App.test.tsx
@@ -0,0 +1,14 @@
+import { describe, test, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+describe('<App />', () => {
+  test('App mounts properly', () => {
+    const wrapper = render(<App />)
+    expect(wrapper).toBeTruthy()
+
+    // Get by text using the React testing library
+    const text = screen.getByText(/Movie Tune/i)
+    expect(text).toBeInTheDocument()
+  })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['src/setupTest.ts']
+  },
   server: {
     port: 3000
   },
